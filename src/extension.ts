c7b562190c8c809d0a1c077bfed9b302cf1f5fda
Beniamino Ventura <benia@protonmail.com>
2024-01-10 17:44:31 +0100

    root

---
+++ src/extension.ts
@@ -0,0 +1,47 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "helloworld-vsce-webview" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('helloworld-vsce-webview.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from helloworld-vsce-webview!');
+	});
+
+	context.subscriptions.push(disposable);
+
+	const test = vscode.commands.registerCommand("helloworld-vsce-webview.open", () => {
+		const panel = vscode.window.createWebviewPanel(
+			"helloworld-vsce",
+			"HelloWorldWebview",
+			vscode.ViewColumn.Active,
+			{}
+		);
+
+		panel.webview.html = `
+			<html>
+				<head>
+				</head>
+				<body>
+					<h1>Hello World</h1>
+				</body>
+			</html>
+		`;
+	});
+
+	context.subscriptions.push(test);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
